3b9f2cd8b300e7d5c21b474abbeb568b1b37f647
Joel Schecheleski <joel.jsp@gmail.com>
2022-09-16 14:05:06 -0300

    0003 - Adicionado botão de logar

--- lib/src/auth/sign_in_screen.dart
+++ lib/src/auth/sign_in_screen.dart
@@ -7,35 +7,75 @@ class SignInScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.green,
+      backgroundColor: const Color.fromRGBO(197, 253, 191, 1.0),
+      //Color.fromRGBO(210, 210, 167, 1.0),
+
       body: Column(
         children: [
           Expanded(
-              child: Container(color: Colors.green)
-          ),
+              child: Container(
+            height: 200,
+            width: double.infinity,
+            decoration: const BoxDecoration(
+              color: Color.fromRGBO(197, 253, 191, 1.0),
+              image: DecorationImage(
+                  image: AssetImage("assets/images/grocery_with_text.png"),
+                  fit: BoxFit.cover),
+            ),
+          )),
           Expanded(
               child: Container(
-                padding: const EdgeInsets.symmetric(
-                  horizontal: 32,
-                  vertical: 40
+            padding: const EdgeInsets.symmetric(horizontal: 32, vertical: 40),
+            decoration: const BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.vertical(top: Radius.circular(45))),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                // Email
+                const CustomTextField(
+                  icon: Icons.email,
+                  label: 'Email',
                 ),
-                decoration: const BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: BorderRadius.vertical(
-                    top: Radius.circular(45)
-                  )
-                ),
-                child: Column(
-                  children: [
-                    // Email
-                    CustomTextField(icon: Icons.email, label: 'Email'),
 
-                    // Senha
-                    CustomTextField(icon: Icons.lock, label: 'Password', isSecret: true)
-                  ],
+                // Senha
+                const CustomTextField(
+                  icon: Icons.lock,
+                  label: 'Password',
+                  isSecret: true,
                 ),
-              )
-          )
+
+                // Entrar
+                SizedBox(
+                  height: 50,
+                  child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(18),
+                      ),
+                    ),
+                    onPressed: () {},
+                    child: const Text('Entrar',
+                        style: TextStyle(
+                          fontSize: 18,
+                        )),
+                  ),
+                ),
+                Align(
+                  alignment: Alignment.centerRight,
+                  child: TextButton(
+                    onPressed: () {},
+                    child: const Text(
+                      'Esqueceu a senha?',
+                      style: TextStyle(
+                        color: Colors.red,
+                      ),
+                    ),
+                  ),
+                )
+              ],
+            ),
+          ))
         ],
       ),
     );
