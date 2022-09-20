655691e3ae8e220b6efde99bbad1e9d0257a4e39
Joel Schecheleski <joel.jsp@gmail.com>
2022-09-20 11:24:28 -0300

    0007 - Criado tela básica para todas as demais chamadas

--- lib/src/auth/sign_up_screen.dart
+++ lib/src/auth/sign_up_screen.dart
@@ -1,9 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:greengrocer/src/auth/components/custom_text_field.dart';
 import 'package:greengrocer/src/config/custom_colors.dart';
+import 'package:mask_text_input_formatter/mask_text_input_formatter.dart';
 
 class SignUpScreen extends StatelessWidget {
-  const SignUpScreen({Key? key}) : super(key: key);
+  SignUpScreen({Key? key}) : super(key: key);
+
+  final cpfFormatter = MaskTextInputFormatter(
+    mask: '###.###.###-##',
+    filter: {'#': RegExp(r'[0-9]')},
+  );
+
+  final phoneFormatter = MaskTextInputFormatter(
+    mask: '(##) # ####-####',
+    filter: {'#': RegExp(r'[0-9]')},
+  );
 
   @override
   Widget build(BuildContext context) {
@@ -15,82 +26,104 @@ class SignUpScreen extends StatelessWidget {
         child: SizedBox(
           height: size.height,
           width: size.width,
-          child: Column(
+          child: Stack(
             children: [
-              const Expanded(
-                child: Center(
-                  child: Text(
-                    'Cadastro',
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 35,
-                    ),
-                  ),
-                ),
-              ),
-
-              // FORMULÁRIO
-              Container(
-                padding: const EdgeInsets.symmetric(horizontal: 32, vertical: 40),
-                decoration: const BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: BorderRadius.vertical(
-                    top: Radius.circular(45),
-                  ),
-                ),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.stretch,
-                  children: [
-                    // Email
-                    const CustomTextField(
-                      icon: Icons.email,
-                      label: 'Email',
-                    ),
-                    // Senha
-                    const CustomTextField(
-                      icon: Icons.lock,
-                      label: 'Password',
-                      isSecret: true,
-                    ),
-                    // Nome
-                    const CustomTextField(
-                      icon: Icons.person,
-                      label: 'Name',
-                      isSecret: false,
-                    ),
-                    // Celular
-                    const CustomTextField(
-                      icon: Icons.phone,
-                      label: 'Cellphone',
-                      isSecret: false,
-                    ),
-                    // CPF
-                    const CustomTextField(
-                      icon: Icons.file_copy,
-                      label: 'Cpf',
-                      isSecret: false,
-                    ),
-                    // Botão de cadastrar
-                    SizedBox(
-                      height: 50,
-                      child: ElevatedButton(
-                        style: ElevatedButton.styleFrom(
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(18),
-                          ),
-                        ),
-                        onPressed: () {},
-                        child: const Text(
-                          'Cadastrar usuário',
-                          style: TextStyle(
-                            fontSize: 18,
-                          ),
+              Column(
+                children: [
+                  const Expanded(
+                    child: Center(
+                      child: Text(
+                        'Cadastro',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 35,
                         ),
                       ),
                     ),
-                  ],
-                ),
+                  ),
+
+                  // FORMULÁRIO
+                  Container(
+                    padding: const EdgeInsets.symmetric(
+                        horizontal: 32, vertical: 40),
+                    decoration: const BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.vertical(
+                        top: Radius.circular(45),
+                      ),
+                    ),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.stretch,
+                      children: [
+                        // Email
+                        const CustomTextField(
+                          icon: Icons.email,
+                          label: 'Email',
+                        ),
+                        // Senha
+                        const CustomTextField(
+                          icon: Icons.lock,
+                          label: 'Password',
+                          isSecret: true,
+                        ),
+                        // Nome
+                        const CustomTextField(
+                          icon: Icons.person,
+                          label: 'Name',
+                          isSecret: false,
+                        ),
+                        // Celular
+                        CustomTextField(
+                          icon: Icons.phone,
+                          label: 'Cellphone',
+                          isSecret: false,
+                          inputFormatter: [phoneFormatter],
+                        ),
+                        // CPF
+                        CustomTextField(
+                          icon: Icons.file_copy,
+                          label: 'Cpf',
+                          isSecret: false,
+                          inputFormatter: [cpfFormatter],
+                        ),
+                        // Botão de cadastrar
+                        SizedBox(
+                          height: 50,
+                          child: ElevatedButton(
+                            style: ElevatedButton.styleFrom(
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(18),
+                              ),
+                            ),
+                            onPressed: () {},
+                            child: const Text(
+                              'Cadastrar usuário',
+                              style: TextStyle(
+                                fontSize: 18,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
               ),
+              Positioned(
+                left: 10,
+                top: 10,
+                child: SafeArea(
+                  child: IconButton(
+                    onPressed: () {
+                      Navigator.of(context).pop();
+                    },
+                    icon: const Icon(
+                      Icons.arrow_back_ios,
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+              )
             ],
           ),
         ),
---
+++ lib/src/base/base_screen.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+
+class BaseScreen extends StatefulWidget {
+  const BaseScreen({Key? key}) : super(key: key);
+
+  @override
+  State<BaseScreen> createState() => _BaseScreenState();
+}
+
+class _BaseScreenState extends State<BaseScreen> {
+  int currentIndex = 0;
+  final pageController = PageController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: PageView(
+        physics: const NeverScrollableScrollPhysics(),
+        controller: pageController,
+        children: [
+          Container(color: Colors.red, child: Text('Tela 1')),
+          Container(color: Colors.yellow, child: Text('Tela 2')),
+          Container(color: Colors.blue, child: Text('Tela 3')),
+          Container(color: Colors.purple, child: Text('Tela 4'))
+        ],
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: currentIndex,
+        onTap: (index) {
+          setState(() {
+            currentIndex = index;
+            pageController.jumpToPage(currentIndex);
+          });
+        },
+        type: BottomNavigationBarType.fixed,
+        backgroundColor: Colors.green,
+        selectedItemColor: Colors.white,
+        unselectedItemColor: Colors.white.withAlpha(100),
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home_outlined),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.shopping_cart_outlined),
+            label: 'Carinho',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.list),
+            label: 'Pedidos',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person_outline),
+            label: 'Perfil',
+          ),
+        ],
+      ),
+    );
+  }
+}
