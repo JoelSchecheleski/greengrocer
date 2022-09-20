6e533b9197c671c185c16ad59194632e38318755
Joel Schecheleski <joel.jsp@gmail.com>
2022-09-20 17:22:45 -0300

    0014 - Adicionado tile para produtos e botÃão para adicioar ao carinho

--- lib/src/pages/home/home_tab_screen.dart
+++ lib/src/pages/home/home_tab_screen.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:greengrocer/src/config/custom_colors.dart';
 import 'package:greengrocer/src/pages/home/components/category_tile.dart';
 import 'package:greengrocer/src/config/app_data.dart' as appData;
+import 'package:greengrocer/src/pages/home/components/item_tile.dart';
 
 class HomeTabScreen extends StatefulWidget {
   const HomeTabScreen({Key? key}) : super(key: key);
@@ -137,9 +138,7 @@ class _HomeTabScreenState extends State<HomeTabScreen> {
               ),
               itemCount: appData.items.length,
               itemBuilder: (_, index) {
-                return Container(
-                  color: Colors.red,
-                );
+                return ItemTile(item: appData.items[index],);
               },
             ),
           ),
